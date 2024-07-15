d5e09b08b5a52fb874cecfeb6d182efb9b05ff28
Sudharsanan-Grl <121048921+sudharsanan-velmurugan@users.noreply.github.com>
2024-07-15 16:58:41 +0530

    Finished React Router Project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Posts from "./Components/Posts";
+import PostDetails from "./Components/PostDetails";
+import Navbar from "./Components/Navbar";
 
-const App = ()=>{
-  return(
-    <div>
-      App  Harish
-    </div>
-  )
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/posts/:postId" element={<PostDetails />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
